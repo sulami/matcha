d77ef6255beb318b6c057960452b6155cad1ef43
Robin Schroer <git@sulami.xyz>
2024-01-07 16:11:51 +0900

    Add a state table for workspaces
    Also default installed packages to the global workspace.

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS meta (
     key TEXT NOT NULL,
     value TEXT NOT NULL,
+
     PRIMARY KEY (key)
 );
 INSERT INTO meta (key, value) VALUES ('schema_version', '1');
@@ -9,13 +10,17 @@ CREATE TABLE IF NOT EXISTS installed_packages (
     name TEXT NOT NULL,
     version TEXT NOT NULL,
     requested_version TEXT NOT NULL,
+    workspace TEXT NOT NULL DEFAULT 'global',
+
     PRIMARY KEY (name, version)
+    FOREIGN KEY (workspace) REFERENCES workspaces (name) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS registries (
     name TEXT NOT NULL,
     uri TEXT NOT NULL,
     last_fetched DATETIME,
+
     PRIMARY KEY (name)
 );
 
@@ -28,8 +33,15 @@ CREATE TABLE IF NOT EXISTS known_packages (
     source TEXT,
     build TEXT,
     artifacts TEXT,
-
     registry TEXT NOT NULL,
+
     PRIMARY KEY (name, version),
     FOREIGN KEY (registry) REFERENCES registries (name) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS workspaces (
+    name TEXT NOT NULL,
+
+    PRIMARY KEY (name)
+);
+INSERT INTO workspaces (name) VALUES ('global');
