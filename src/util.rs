17a56d2927ec768e2ad40e285037a48763b19d44
Robin Schroer <git@sulami.xyz>
2024-01-11 17:36:42 +0900

    Add a test for is_file_system_safe

--- src/util.rs
+++ src/util.rs
@@ -3,3 +3,20 @@ pub fn is_file_system_safe(s: &str) -> bool {
     s.chars()
         .all(|c| c.is_ascii_alphanumeric() || c == '-' || c == '_' || c == '.')
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_file_system_safe() {
+        assert!(is_file_system_safe("foo"));
+        assert!(is_file_system_safe("foo-bar"));
+        assert!(is_file_system_safe("foo_bar"));
+        assert!(is_file_system_safe("foo.bar"));
+
+        assert!(!is_file_system_safe("foo/bar"));
+        assert!(!is_file_system_safe("foo bar"));
+        assert!(!is_file_system_safe(r"foo\bar"));
+    }
+}
